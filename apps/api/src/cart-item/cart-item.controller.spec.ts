6f5ee0acb2488a577440b93bba72590f20c355b6
GuillaumeM10 <guillaumemengelatte@hotmail.fr>
2025-08-12 23:23:16 +0200

    feat: cart api test

--- apps/api/src/cart-item/cart-item.controller.spec.ts
+++ apps/api/src/cart-item/cart-item.controller.spec.ts
@@ -1,7 +1,12 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, type TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
+import { CartService } from '../cart/cart.service';
+import { ValidTokenService } from '../valid-token/valid-token.service';
 import { CartItemController } from './cart-item.controller';
 import { CartItemService } from './cart-item.service';
+import { CartItemEntity } from './entities/cart-item.entity';
 
 describe('CartItemController', () => {
   let controller: CartItemController;
@@ -9,7 +14,39 @@ describe('CartItemController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CartItemController],
-      providers: [CartItemService],
+      providers: [
+        CartItemService,
+        {
+          provide: getRepositoryToken(CartItemEntity),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            find: jest.fn(),
+            createQueryBuilder: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: CartService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            verifyAsync: jest.fn(),
+          },
+        },
+        {
+          provide: ValidTokenService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CartItemController>(CartItemController);
